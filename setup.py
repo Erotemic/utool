772c9d42ac45ffe8744df49b0856b145bbd46ece
Jon Crall <crallj@rpi.edu>
2014-05-23 16:43:04 -0400

    wip

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 from __future__ import absolute_import, division, print_function
-from utool.util_setup import setuptools_setup
-import utool
+from setuptools import setup
 
 
 INSTALL_REQUIRES = [
@@ -9,15 +8,16 @@ INSTALL_REQUIRES = [
 ]
 
 if __name__ == '__main__':
-    setuptools_setup(
-        setup_fpath=__file__,
-        module=utool,
+    setup(
+        name='utool',
+        version='1.0.0.dev1',
         description='Useful utility tools for you!',
         url='https://github.com/Erotemic/utool',
         packages=['utool'],
         author='Jon Crall',
         author_email='erotemic@gmail.com',
         keywords='',
+        install_requires=INSTALL_REQUIRES,
         package_data={},
         classifiers=[],
     )
