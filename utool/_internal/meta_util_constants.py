119210e201383dfeb3578658672c7bb0ad855aa7
Jon Crall <crallj@rpi.edu>
2014-06-19 14:07:50 -0400

    wip

---
+++ utool/_internal/meta_util_constants.py
@@ -0,0 +1,4 @@
+from __future__ import absolute_import, division, print_function
+global_cache_fname = 'global_cache.shelf'
+global_cache_dname = 'global_cache'
+default_appname = 'utool'
---
+++ utool/_internal/meta_util_cplat.py
@@ -0,0 +1,27 @@
+from __future__ import absolute_import, division, print_function
+import sys
+from os.path import normpath, expanduser, join
+
+WIN32  = sys.platform.startswith('win32')
+LINUX  = sys.platform.startswith('linux')
+DARWIN = sys.platform.startswith('darwin')
+
+
+def get_resource_dir():
+    """ Returns a directory which should be writable for any application """
+    if WIN32:
+        return normpath(expanduser('~/AppData/Roaming'))
+    if LINUX:
+        return normpath(expanduser('~/.config'))
+    if DARWIN:
+        return normpath(expanduser('~/Library/Application Support'))
+
+
+def get_app_resource_dir(*args):
+    """ Returns a writable directory for an application
+    Input: appname - the name of the application
+           *args, - any other subdirectories may be specified
+    """
+    if len(args) == 0:
+        raise AssertionError('Missing appname. The first argument the application name')
+    return join(get_resource_dir(), *args)
