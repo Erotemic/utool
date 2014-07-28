62276a2ee3f38fc87f3fc867497bc9e36aef64d2
Jon Crall <crallj@rpi.edu>
2014-07-28 18:29:16 -0400

    Pushed on cyth_script. Used astor for Visitor functionality

--- cyth/cyth_script.py
+++ cyth/cyth_script.py
@@ -11,54 +11,32 @@ import utool
 import sys
 from os.path import splitext, isfile
 import ast
+#import codegen  # NOQA
+import astor
 
 #class CythTransformer(ast.NodeTransformer):
 #    #
 
-sample_code = """
-#from __future__ import print_function
-def foo(x, y):
-        '''
-        <CYTH>
-        cdef:
-            long x
-            long y
-        </CYTH>
-        '''
-        for i in range(42):
-            x += 1
-            print(x)
-        return x + y
-"""
 
-class CythVisitor(ast.NodeVisitor):
+#BASE_CLASS = codegen.SourceGenerator
+#BASE_CLASS = ast.NodeVisitor
+#BASE_CLASS = astor.ExplicitNodeVisitor
+BASE_CLASS = astor.codegen.SourceGenerator
+
+
+# See astor/codegen.py for details
+# https://github.com/berkerpeksag/astor/blob/master/astor/codegen.py
+
+
+class CythVisitor(BASE_CLASS):
     indent_level = 0
-    emit = sys.stdout.write # instance variable to allow easily changing where it outputs to
-#    suffixlist = ["ClassDef", "Return", "Delete", "Assign", "AugAssign", "Print", "For", "While", "If", "With", "Raise", "TryExcept", "TryFinally", "Assert", "Import", "ImportFrom", "Exec", "Global", "Expr", "Pass", "Break", "Continue"]
-#    def __getattr__(self, key):
-#        for suffix in [suffixlist]:
-#            if key == visit + suffix:
-#                return self.visit_genericthingy
-#    def visit_genericthingy(self, node):
-#        self.emit()
-    def prefix(self):
-        return " " * self.indent_level
-    def indent(self):
-        self.indent_level += 4
-    def dedent(self):
-        self.indent_level -= 4
-#    def expr_to_string(self, node):
-#        c = node.__class__
-#        if c == ast.Str:
-#            return node.s
-#        elif c == ast.Num:
-#            return str(node.expr)
-#        elif c == ast.Expr:
-#            return expr_to_string(c.value)
-#        else:
-#            assert False, "Unknown type in expr_to_string()"
-    def visit_Module(self, node):
-        return ''.join(map(self.visit, node.body))
+    emit = sys.stdout.write
+
+    def __init__(self, indent_with=' ' * 4, add_line_information=False):
+        super(CythVisitor, self).__init__(indent_with, add_line_information)
+
+    def get_result(self):
+        return ''.join(self.result)
 
     def process_args(self, args, vararg, kwarg, defaults=None):
         processed_argslist = map(self.visit, args)
@@ -68,65 +46,181 @@ class CythVisitor(ast.NodeVisitor):
             processed_argslist.append('**%s' % kwarg)
         if defaults is not None:
             first_non_default = len(args) - len(defaults)
-            for (i, de) in enumerate(defaults):
-                processed_argslist[first_non_default+i] += '=%s' % self.visit(defaults[i])
+            for (ix, de) in enumerate(defaults):
+                processed_argslist[first_non_default + ix] += '=%s' % self.visit(defaults[ix])
         return processed_argslist
 
+    def signature(self, node, typedict={}):
+        want_comma = []
+
+        def write_comma():
+            if want_comma:
+                self.write(', ')
+            else:
+                want_comma.append(True)
+
+        def loop_args(args, defaults):
+            padding = [None] * (len(args) - len(defaults))
+            for arg, default in zip(args, padding + defaults):
+                if arg.id in typedict:
+                    arg_ = typedict[arg.id] + ' ' + arg.id
+                else:
+                    arg_ = arg
+                self.write(write_comma, arg_)
+                self.conditional_write('=', default)
+
+        loop_args(node.args, node.defaults)
+        self.conditional_write(write_comma, '*', node.vararg)
+        self.conditional_write(write_comma, '**', node.kwarg)
+
+        kwonlyargs = getattr(node, 'kwonlyargs', None)
+        if kwonlyargs:
+            if node.vararg is None:
+                self.write(write_comma, '*')
+            loop_args(kwonlyargs, node.kw_defaults)
+
+    def parse_cythdef(self, cyth_def):
+        """ Hacky string manipulation parsing """
+        typedict = {}
+        cdef_mode = False
+        current_indent = 0
+        for line_ in cyth_def.splitlines():
+            indent_level = len(line_) - len(line_.lstrip())
+            # Check indentation
+            if indent_level > current_indent:
+                current_indent = indent_level
+            elif indent_level > current_indent:
+                current_indent = indent_level
+                cdef_mode = False
+            line = line_.strip()
+            if line.startswith('#'):
+                continue
+            if len(line) == 0:
+                continue
+            # parse cdef
+            if line.startswith('cdef:'):
+                cdef_mode = True
+                continue
+            if cdef_mode or line.startswith('cdef '):
+                assign_str = line.replace('cdef ', '')
+                pos = assign_str.rfind(' ')
+                type_ = assign_str[:pos]
+                varstr = assign_str[(pos + 1):]
+                typedict[varstr] = type_
+        return typedict
+
     def visit_FunctionDef(self, node):
-        processed_argslist = self.process_args(node.args.args, node.args.vararg, node.args.kwarg, node.args.defaults)
-        output = "%sdef %s(%s):\n" % (self.prefix(), node.name, ', '.join(processed_argslist))
-        self.indent()
-        for n in node.body:
-            output += self.prefix() + self.visit(n) + '\n'
-        self.dedent()
-        return output
+        #super(CythVisitor, self).visit_FunctionDef(node)
+        has_cython = False
+        replace = False
+        defines = False
+        new_body = []
+        typedict = {}
+        cyth_def = ''
+        for stmt in node.body:
+            if isinstance(stmt, ast.Expr) and isinstance(stmt.value, ast.Str):
+                #print('found comment_str')
+                comment_str = stmt.value.s.strip()
+                if comment_str.startswith('<CYTH'):
+                    print('func: %s has cyth tags' % (node.name,))
+                    def_tag = '<CYTH>'
+                    end_tag = '</CYTH>'
+                    repl_tag = '<CYTH:REPLACE>'
+                    if comment_str.startswith(def_tag):
+                        start_tag = def_tag
+                        defines = True
+                    elif comment_str.startswith(repl_tag):
+                        start_tag = repl_tag
+                        replace = True
+                    cyth_def = comment_str.replace(start_tag, '').replace(end_tag, '')
+                    if replace:
+                        cyth_def = utool.unindent(cyth_def)
+                    if defines:
+                        typedict = self.parse_cythdef(cyth_def)
+                    has_cython = True
+            else:
+                new_body.append(stmt)
+        if has_cython:
+            if not replace:
+                #self.decorators(node, 2)
+                self.newline(extra=1)
+                self.statement(node, 'def %s(' % node.name)
+                self.signature(node.args, typedict=typedict)
+                self.write(')')
+                if getattr(node, 'returns', None) is not None:
+                    self.write(' ->', node.returns)
+                self.write(':')
+                self.write(cyth_def)
+                self.body(new_body)
+            else:
+                self.newline(extra=1)
+                self.write(cyth_def)
+    #    processed_argslist = self.process_args(node.args.args, node.args.vararg, node.args.kwarg, node.args.defaults)
+    #    output = "%sdef %s(%s):\n" % (self.prefix(), node.name, ', '.join(processed_argslist))
+    #    self.indent()
+    #    #print(codegen.to_source(node.body))
+    #    for n in node.body:
+    #        output += self.prefix() + self.visit(n) + '\n'
+    #    self.dedent()
+    #    return output
 
-    def visit_Str(self, node):
-        return repr(node.s)
+    #def prefix(self):
+    #    return " " * self.indent_level
 
-    def visit_Num(self, node):
-        return str(node.n)
+    #def indent(self):
+    #    self.indent_level += 4
 
-    def visit_Expr(self, node):
-        return self.visit(node.value)
+    #def dedent(self):
+    #    self.indent_level -= 4
 
-    def visit_Return(self, node):
-        return ('return %s' % (self.visit(node.value)))
+    #def visit_Module(self, node):
+    #    return ''.join(map(self.visit, node.body))
 
-    def visit_BinOp(self, node):
-        return ('%s %s %s' % (self.visit(node.left), self.visit(node.op), self.visit(node.right)))
+    #def visit_Str(self, node):
+    #    return repr(node.s)
 
-    def visit_Name(self, node):
-        return node.id
+    #def visit_Num(self, node):
+    #    return str(node.n)
 
-    def visit_Add(self, node):
-        return '+'
+    #def visit_Expr(self, node):
+    #    return self.visit(node.value)
 
-    def visit_For(self, node):
-        output = 'for %s in %s:\n' % (self.visit(node.target), self.visit(node.iter))
-        self.indent()
-        for n in node.body:
-            output += '%s%s\n' % (self.prefix(), self.visit(n))
-        self.dedent()
-        return output
+    #def visit_Return(self, node):
+    #    return ('return %s' % (self.visit(node.value)))
 
-    def visit_Call(self, node):
-        output = '%s(%s)' % (self.visit(node.func), ', '.join(self.process_args(node.args, node.starargs, node.kwargs)))
-        return output
+    #def visit_BinOp(self, node):
+    #    return ('%s %s %s' % (self.visit(node.left), self.visit(node.op), self.visit(node.right)))
 
-    def visit_AugAssign(self, node):
-        return '%s %s= %s' % (self.visit(node.target), self.visit(node.op), self.visit(node.value))
-#
-#    def visit_Print(self, node):
-#        pass
+    #def visit_Name(self, node):
+    #    return node.id
 
-    def generic_visit(self, node):
-        raise NotImplementedError("Not implemented for type %r" % (node.__class__))
+    #def visit_Add(self, node):
+    #    return '+'
 
-def cyth_process(pysource):
-    ast_root = ast.parse(pysource)
-    #
+    #def visit_For(self, node):
+    #    output = 'for %s in %s:\n' % (self.visit(node.target), self.visit(node.iter))
+    #    self.indent()
+    #    for n in node.body:
+    #        output += '%s%s\n' % (self.prefix(), self.visit(n))
+    #    self.dedent()
+    #    return output
 
+    #def visit_Call(self, node):
+    #    output = '%s(%s)' % (self.visit(node.func), ', '.join(self.process_args(node.args, node.starargs, node.kwargs)))
+    #    return output
+
+    #def visit_AugAssign(self, node):
+    #    return '%s %s= %s' % (self.visit(node.target), self.visit(node.op), self.visit(node.value))
+
+    #def visit_Print(self, node):
+    #    pass
+
+    #def generic_visit(self, node):
+    #    raise NotImplementedError("Not implemented for type %r" % (node.__class__))
+
+
+#def cyth_process(pysource):
+#    ast_root = ast.parse(pysource)
 
 
 def find_cyth_tags(py_text):
--- cyth/simple_test.py
+++ cyth/simple_test.py
@@ -1,8 +1,58 @@
+#!/usr/bin/python2.7
 from __future__ import absolute_import, division, print_function
 import ast
-from cyth_script import *
-pt = ast.parse(sample_code)
-print(ast.dump(pt))
-a = CythVisitor()
-print(a.visit(pt))
+#import codegen
+import astor  # NOQA;
+from cyth_script import *  # NOQA
 
+sample_code = """
+#from __future__ import print_function
+def foo(x, y):
+        '''
+        <CYTH>
+        cdef:
+            long x
+            long y
+        </CYTH>
+        '''
+        for i in range(42):
+            x += 1
+            print(x)
+        return x + y
+"""
+
+pt = ast.parse(sample_code)
+#print(astor.to_source(pt))
+print('---------------------')
+print('Abstract Syntax Tree:')
+print(ast.dump(pt))
+print('---------------------\n')
+
+print('---------------------')
+print('Cyth Visit Simple')
+
+visitor = CythVisitor()
+print(visitor.visit(pt))
+
+
+# More complicated test
+import vtool
+import utool
+from os.path import join
+py_fpath = join(vtool.__path__[0], 'keypoint.py')
+py_text = utool.read_from(py_fpath)
+
+parse_tree = ast.parse(py_text)
+print(py_fpath)
+
+print('---------------------')
+#print('Abstract Syntax Tree 2:')
+#print(ast.dump(parse_tree))
+
+print('---------------------')
+print('Cyth Visit Complex 2')
+visitor2 = CythVisitor()
+visitor2.visit(parse_tree)
+print(visitor2.get_result())
+
+#print(astor.to_source(parse_tree))
