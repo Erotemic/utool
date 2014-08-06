ee719d6f6c193f4da580a89db7ef3a05a5b56bec
Jon Crall <crallj@rpi.edu>
2014-08-06 14:07:07 -0400

    fixed importer to return correct module

--- cyth/cyth_importer.py
+++ cyth/cyth_importer.py
@@ -10,8 +10,8 @@ WITH_CYTH = utool.get_flag('--cyth')
 WITH_CYTH = not utool.get_flag('--nocyth')
 
 
-def pkg_submodule_split(modname):
-    cyth_modname = cyth_helpers.get_cyth_name(modname)
+def pkg_submodule_split(pyth_modname):
+    cyth_modname = cyth_helpers.get_cyth_name(pyth_modname)
     # Break module name into package and submodule
     if cyth_modname.find('.') > -1:
         components = cyth_modname.split('.')
@@ -20,7 +20,7 @@ def pkg_submodule_split(modname):
     else:
         pkgname = cyth_modname
         fromlist = []
-    return pkgname, fromlist
+    return pkgname, fromlist, cyth_modname
 
 
 def import_cyth(pyth_modname):
@@ -31,8 +31,8 @@ def import_cyth(pyth_modname):
     try:
         if not WITH_CYTH:
             raise ImportError('NO_CYTH')
-        pkgname, fromlist = pkg_submodule_split(pyth_modname)
-        cyth_mod = __import__(pkgname, globals(), locals(), fromlist=fromlist, level=0)
+        pkgname, fromlist, cyth_modname = pkg_submodule_split(pyth_modname)
+        cyth_mod = __import__(cyth_modname, globals(), locals(), fromlist=fromlist, level=0)
 
         # TODO: Get list of cythonized funcs and return them
 
