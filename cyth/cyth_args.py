9ca8221e395ea3bd92e653e8c7074135c40e6ee0
Avi Weinstock <aweinstock314@gmail.com>
2014-08-18 10:48:21 -0400

    Moved cyth to its own repository.

--- cyth/cyth_args.py
+++
@@ -1,6 +0,0 @@
-from __future__ import absolute_import, division, print_function
-import utool
-
-DYNAMIC = not utool.get_flag('--nodyn')
-WITH_CYTH = not utool.get_flag('--nocyth')
-CYTH_WRITE = utool.get_flag('--cyth-write')
--- cyth/cyth_decorators.py
+++
@@ -1,4 +0,0 @@
-"""
-python -c "import doctest, cyth; print(doctest.testmod(cyth.cyth_decorators))"
-"""
-from __future__ import absolute_import, division, print_function
