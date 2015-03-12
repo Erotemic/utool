411b4100c28425b1e2b74db832d75dad18713b08
joncrall <erotemic@gmail.com>
2015-03-12 13:18:23 -0400

    wip

--- utool/_internal/meta_util_iter.py
+++ utool/_internal/meta_util_iter.py
@@ -3,6 +3,28 @@ import six
 
 
 def ensure_iterable(obj):
+    r"""
+    Args:
+        obj (scalar or iterable):
+
+    Returns:
+        it3erable: obj if it was iterable otherwise [obj]
+
+    CommandLine:
+        python -m utool._internal.meta_util_iter --test-ensure_iterable
+
+    Example:
+        >>> # DISABLE_DOCTEST
+        >>> from utool._internal.meta_util_iter import *  # NOQA
+        >>> # build test data
+        >>> obj_list = [3, [3], '3', (3,), [3,4,5]]
+        >>> # execute function
+        >>> result = [ensure_iterable(obj) for obj in obj_list]
+        >>> # verify results
+        >>> result = str(result)
+        >>> print(result)
+        [[3], [3], ['3'], (3,), [3, 4, 5]]
+    """
     if isiterable(obj):
         return obj
     else:
@@ -13,6 +35,26 @@ def isiterable(obj):
     """
     Returns if the object can be iterated over and is NOT a string
     # TODO: implement isscalar similar to numpy
+
+    Args:
+        obj (scalar or iterable):
+
+    Returns:
+        bool:
+
+    CommandLine:
+        python -m utool._internal.meta_util_iter --test-isiterable
+
+    Example:
+        >>> # DISABLE_DOCTEST
+        >>> from utool._internal.meta_util_iter import *  # NOQA
+        >>> # build test data
+        >>> obj_list = [3, [3], '3', (3,), [3,4,5]]
+        >>> # execute function
+        >>> result = [isiterable(obj) for obj in obj_list]
+        >>> # verify results
+        >>> print(result)
+        [False, True, False, True, True]
     """
     try:
         iter(obj)
@@ -23,3 +65,14 @@ def isiterable(obj):
 
 
 #def isscalar(obj):
+if __name__ == '__main__':
+    """
+    CommandLine:
+        python -m utool._internal.meta_util_iter
+        python -m utool._internal.meta_util_iter --allexamples
+        python -m utool._internal.meta_util_iter --allexamples --noface --nosrc
+    """
+    import multiprocessing
+    multiprocessing.freeze_support()  # for win32
+    import utool as ut  # NOQA
+    ut.doctest_funcs()
