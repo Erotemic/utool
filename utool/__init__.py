c06d4a35e825a9146768b8b43420048d6d9c2b0f
joncrall <erotemic@gmail.com>
2015-06-15 17:25:29 -0400

    wip

--- utool/__init__.py
+++ utool/__init__.py
@@ -16,7 +16,8 @@ import textwrap
 
 #__fun_version__ = '(.878 + .478i)'
 #__version__ = '1.0.0.dev1'
-__version__ = '1.1.0.dev1'
+#__version__ = '1.1.0.dev1'
+__version__ = '1.1.1.dev1'
 
 __DYNAMIC__ = not '--nodyn' in sys.argv
 #__DYNAMIC__ = '--dyn' in sys.argv
