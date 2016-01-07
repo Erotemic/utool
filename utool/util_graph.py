758247a4560177a1178625a0c0b1e6580f75e03b
joncrall <erotemic@gmail.com>
2016-01-07 16:55:30 -0500

    wip

---
+++ utool/util_graph.py
@@ -0,0 +1,291 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, division, print_function, unicode_literals
+import numpy as np
+from utool import util_inject
+(print, rrr, profile) = util_inject.inject2(__name__, '[depgraph_helpers]')
+
+
+def dict_depth(dict_, accum=0):
+    if not isinstance(dict_, dict):
+        return accum
+    return max([dict_depth(val, accum + 1)
+                for key, val in dict_.items()])
+
+
+def paths_to_root(tablename, root, child_to_parents):
+    """
+
+    Example:
+        >>> # ENABLE_DOCTEST
+        >>> from utool.util_graph import *  # NOQA
+        >>> import utool as ut
+        >>> child_to_parents = {
+        >>>     'chip': ['dummy_annot'],
+        >>>     'chipmask': ['dummy_annot'],
+        >>>     'descriptor': ['keypoint'],
+        >>>     'fgweight': ['keypoint', 'probchip'],
+        >>>     'keypoint': ['chip'],
+        >>>     'notch': ['dummy_annot'],
+        >>>     'probchip': ['dummy_annot'],
+        >>>     'spam': ['fgweight', 'chip', 'keypoint']
+        >>> }
+        >>> root = 'dummy_annot'
+        >>> tablename = 'fgweight'
+        >>> to_root = paths_to_root(tablename, root, child_to_parents)
+        >>> result = ut.repr3(to_root)
+        >>> print(result)
+        {
+            'keypoint': {
+                'chip': {
+                        'dummy_annot': None,
+                    },
+            },
+            'probchip': {
+                'dummy_annot': None,
+            },
+        }
+    """
+    if tablename == root:
+        return None
+    parents = child_to_parents[tablename]
+    return {parent: paths_to_root(parent, root, child_to_parents)
+            for parent in parents}
+
+
+def path_to_leafs(tablename, parent_to_children):
+    children = parent_to_children[tablename]
+    if len(children) == 0:
+        return None
+    return {child: path_to_leafs(child, parent_to_children)
+            for child in children}
+
+
+def get_allkeys(dict_):
+    import utool as ut
+    if not isinstance(dict_, dict):
+        return []
+    subkeys = [[key] + get_allkeys(val)
+               for key, val in dict_.items()]
+    return ut.unique_ordered(ut.flatten(subkeys))
+
+
+def traverse_path(start, end, seen_, allkeys, mat):
+    import utool as ut
+    if seen_ is None:
+        seen_ = set([])
+    index = allkeys.index(start)
+    sub_indexes = np.where(mat[index])[0]
+    if len(sub_indexes) > 0:
+        subkeys = ut.take(allkeys, sub_indexes)
+        # subkeys_ = ut.take(allkeys, sub_indexes)
+        # subkeys = [subkey for subkey in subkeys_
+        #            if subkey not in seen_]
+        # for sk in subkeys:
+        #     seen_.add(sk)
+        if len(subkeys) > 0:
+            return {subkey: traverse_path(subkey, end, seen_, allkeys, mat)
+                    for subkey in subkeys}
+    return None
+
+
+def reverse_path(dict_, root, child_to_parents):
+    """
+    CommandLine:
+        python -m utool.util_graph --exec-reverse_path --show
+
+    Example:
+        >>> # ENABLE_DOCTEST
+        >>> from utool.util_graph import *  # NOQA
+        >>> import utool as ut
+        >>> child_to_parents = {
+        >>>     'chip': ['dummy_annot'],
+        >>>     'chipmask': ['dummy_annot'],
+        >>>     'descriptor': ['keypoint'],
+        >>>     'fgweight': ['keypoint', 'probchip'],
+        >>>     'keypoint': ['chip'],
+        >>>     'notch': ['dummy_annot'],
+        >>>     'probchip': ['dummy_annot'],
+        >>>     'spam': ['fgweight', 'chip', 'keypoint']
+        >>> }
+        >>> to_root = {
+        >>>     'fgweight': {
+        >>>         'keypoint': {
+        >>>                 'chip': {
+        >>>                             'dummy_annot': None,
+        >>>                         },
+        >>>             },
+        >>>         'probchip': {
+        >>>                 'dummy_annot': None,
+        >>>             },
+        >>>     },
+        >>> }
+        >>> reversed_ = reverse_path(to_root, 'dummy_annot', child_to_parents)
+        >>> result = ut.repr3(reversed_)
+        >>> print(result)
+        {
+            'dummy_annot': {
+                'chip': {
+                        'keypoint': {
+                                    'fgweight': None,
+                                },
+                    },
+                'probchip': {
+                        'fgweight': None,
+                    },
+            },
+        }
+    """
+    # Hacky but illustrative
+    # TODO; implement non-hacky version
+    allkeys = get_allkeys(dict_)
+    mat = np.zeros((len(allkeys), len(allkeys)))
+    for key in allkeys:
+        if key != root:
+            for parent in child_to_parents[key]:
+                rx = allkeys.index(parent)
+                cx = allkeys.index(key)
+                mat[rx][cx] = 1
+    end = None
+    seen_ = set([])
+    reversed_ = {root: traverse_path(root, end, seen_, allkeys, mat)}
+    return reversed_
+
+
+def get_levels(dict_, n=0, levels=None):
+    r"""
+    Args:
+        dict_ (dict_):  a dictionary
+        n (int): (default = 0)
+        levels (None): (default = None)
+
+    CommandLine:
+        python -m utool.util_graph --exec-get_levels --show
+
+    Example:
+        >>> # ENABLE_DOCTEST
+        >>> from utool.util_graph import *  # NOQA
+        >>> import utool as ut
+        >>> from_root = {
+        >>>     'dummy_annot': {
+        >>>         'chip': {
+        >>>                 'keypoint': {
+        >>>                             'fgweight': None,
+        >>>                         },
+        >>>             },
+        >>>         'probchip': {
+        >>>                 'fgweight': None,
+        >>>             },
+        >>>     },
+        >>> }
+        >>> dict_ = from_root
+        >>> n = 0
+        >>> levels = None
+        >>> levels_ = get_levels(dict_, n, levels)
+        >>> result = ut.repr2(levels_, nl=1)
+        >>> print(result)
+        [
+            ['dummy_annot'],
+            ['chip', 'probchip'],
+            ['keypoint', 'fgweight'],
+            ['fgweight'],
+        ]
+    """
+    if levels is None:
+        levels_ = [[] for _ in range(dict_depth(dict_))]
+    else:
+        levels_ = levels
+    if dict_ is None:
+        return []
+    for key in dict_.keys():
+        levels_[n].append(key)
+    for val in dict_.values():
+        get_levels(val, n + 1, levels_)
+    return levels_
+
+
+def longest_levels(levels_):
+    r"""
+    Args:
+        levels_ (list):
+
+    CommandLine:
+        python -m utool.util_graph --exec-longest_levels --show
+
+    Example:
+        >>> # ENABLE_DOCTEST
+        >>> from utool.util_graph import *  # NOQA
+        >>> import utool as ut
+        >>> levels_ = [
+        >>>     ['dummy_annot'],
+        >>>     ['chip', 'probchip'],
+        >>>     ['keypoint', 'fgweight'],
+        >>>     ['fgweight'],
+        >>> ]
+        >>> new_levels = longest_levels(levels_)
+        >>> result = ('new_levels = %s' % (ut.repr2(new_levels, nl=1),))
+        >>> print(result)
+        new_levels = [
+            ['dummy_annot'],
+            ['chip', 'probchip'],
+            ['keypoint'],
+            ['fgweight'],
+        ]
+    """
+    return shortest_levels(levels_[::-1])[::-1]
+    # seen_ = set([])
+    # new_levels = []
+    # for level in levels_[::-1]:
+    #     new_level = [item for item in level if item not in seen_]
+    #     seen_ = seen_.union(set(new_level))
+    #     new_levels.append(new_level)
+    # new_levels = new_levels[::-1]
+    # return new_levels
+
+
+def shortest_levels(levels_):
+    r"""
+    Args:
+        levels_ (list):
+
+    CommandLine:
+        python -m utool.util_graph --exec-shortest_levels --show
+
+    Example:
+        >>> # ENABLE_DOCTEST
+        >>> from utool.util_graph import *  # NOQA
+        >>> import utool as ut
+        >>> levels_ = [
+        >>>     ['dummy_annot'],
+        >>>     ['chip', 'probchip'],
+        >>>     ['keypoint', 'fgweight'],
+        >>>     ['fgweight'],
+        >>> ]
+        >>> new_levels = shortest_levels(levels_)
+        >>> result = ('new_levels = %s' % (ut.repr2(new_levels, nl=1),))
+        >>> print(result)
+        new_levels = [
+            ['dummy_annot'],
+            ['chip', 'probchip'],
+            ['keypoint', 'fgweight'],
+        ]
+    """
+    seen_ = set([])
+    new_levels = []
+    for level in levels_:
+        new_level = [item for item in level if item not in seen_]
+        seen_ = seen_.union(set(new_level))
+        if len(new_level) > 0:
+            new_levels.append(new_level)
+    new_levels = new_levels
+    return new_levels
+
+
+if __name__ == '__main__':
+    r"""
+    CommandLine:
+        python -m utool.util_graph --allexamples
+    """
+    import multiprocessing
+    multiprocessing.freeze_support()  # for win32
+    import utool as ut  # NOQA
+    ut.doctest_funcs()
