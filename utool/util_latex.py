e9acbc945b682a94772b9e25a736eabd86d4d6ef
joncrall <crallj@rpi.edu>
2016-04-24 15:42:48 -0400

    wip

--- utool/util_latex.py
+++ utool/util_latex.py
@@ -860,7 +860,10 @@ def get_latex_figure_str(fpath_list, caption_str=None, label_str=None,
         ) % (header_str, graphics_body)
     if caption_str is not None:
         #tabular_body += '\n\caption{\\footnotesize{%s}}' % (caption_str,)
-        figure_body += '\n\caption{%s}' % (caption_str,)
+        if label_str is not None:
+            figure_body += '\n\caption[%s]{%s}' % (label_str, caption_str,)
+        else:
+            figure_body += '\n\caption{%s}' % (caption_str,)
     if label_str is not None:
         figure_body += '\n\label{fig:%s}' % (label_str,)
     #figure_fmtstr = ut.codeblock(
