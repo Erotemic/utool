b27d6c6a2edcdfe40d979cc0814797dbc9c86f0d
joncrall <erotemic@gmail.com>
2016-07-20 10:34:56 -0400

    wip

--- utool/_internal/meta_util_arg.py
+++ utool/_internal/meta_util_arg.py
@@ -42,33 +42,6 @@ if PRINT_INJECT_ORDER:
     msg = '[util_inject] N={N} {modname} is imported by {callername} at lineno={lineno}'.format(**fmtdict)
     builtins.print(msg)
 
-#def get_argval(arg, type_=None, default=None):
-#    arg_after = default
-#    if type_ is bool:
-#        arg_after = False if default is None else default
-#    try:
-#        argx = sys.argv.index(arg)
-#        if argx < len(sys.argv):
-#            if type_ is bool:
-#                arg_after = True
-#            else:
-#                try:
-#                    arg_after_ = sys.argv[argx + 1]
-#                    if type_ in [types.BooleanType] and isinstance(arg_after_, six.string_types):
-#                        if arg_after_.lower() == 'true':
-#                            arg_after_ = True
-#                        elif arg_after_.lower() == 'false':
-#                            arg_after_ = True
-#                    else:
-#                        arg_after_ = type_(arg_after_)
-#                except Exception:
-#                    raise
-#                    pass
-#                arg_after = arg_after_
-#    except Exception:
-#        pass
-#    return arg_after
-
 
 def _try_cast(val, type_):
     if type_ in [types.BooleanType] and isinstance(val, six.string_types):
@@ -88,23 +61,26 @@ def get_argflag(flag):
 # FIXME: on a rrrr this get_argval is taken
 # instead of the correct one from util_arg
 def get_argval(argstr, type_=None, default=None):
-    """ Returns a value of an argument specified on the command line after some flag
+    r"""
+    Returns a value of an argument specified on the command line after some flag
 
-    python -c "import utool; print([(type(x), x) for x in [utool.get_argval('--quest')]])" --quest="holy grail"
-    python -c "import utool; print([(type(x), x) for x in [utool.get_argval('--quest')]])" --quest="42"
-    python -c "import utool; print([(type(x), x) for x in [utool.get_argval('--quest')]])" --quest=42
-    python -c "import utool; print([(type(x), x) for x in [utool.get_argval('--quest')]])" --quest 42
-    python -c "import utool; print([(type(x), x) for x in [utool.get_argval('--quest', float)]])" --quest 42
+    CommandLine:
+        python -c "import utool; print([(type(x), x) for x in [utool.get_argval('--quest')]])" --quest="holy grail"
+        python -c "import utool; print([(type(x), x) for x in [utool.get_argval('--quest')]])" --quest="42"
+        python -c "import utool; print([(type(x), x) for x in [utool.get_argval('--quest')]])" --quest=42
+        python -c "import utool; print([(type(x), x) for x in [utool.get_argval('--quest')]])" --quest 42
+        python -c "import utool; print([(type(x), x) for x in [utool.get_argval('--quest', float)]])" --quest 42
 
-    >>> from utool.util_arg import *  # NOQA
-    >>> import sys
-    >>> sys.argv.extend(['--spam', 'eggs', '--quest=holy grail', '--ans=42'])
-    >>> get_argval('--spam', type_=str, default=None)
-    eggs
-    >>> get_argval('--quest', type_=str, default=None)
-    holy grail
-    >>> get_argval('--ans', type_=int, default=None)
-    42
+    Example:
+        >>> from utool.util_arg import *  # NOQA
+        >>> import sys
+        >>> sys.argv.extend(['--spam', 'eggs', '--quest=holy grail', '--ans=42'])
+        >>> get_argval('--spam', type_=str, default=None)
+        eggs
+        >>> get_argval('--quest', type_=str, default=None)
+        holy grail
+        >>> get_argval('--ans', type_=int, default=None)
+        42
     """
     arg_after = default
     if type_ is bool:
