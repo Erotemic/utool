2364c683bb971fcf85c2208ce3375a8ef79080a8
joncrall <crallj@rpi.edu>
2016-07-31 20:10:36 -0400

    tagging helpers

--- utool/util_tags.py
+++ utool/util_tags.py
@@ -7,6 +7,76 @@ from utool import util_inject
 print, rrr, profile = util_inject.inject2(__name__, '[util_tags]')
 
 
+def build_alias_map(regex_map, tag_vocab):
+    """
+    Constructs explicit mapping. Order of items in regex map matters.
+    Items at top are given preference.
+
+    Example:
+        >>> tags_list = [['t1', 't2'], [], ['t3'], ['t4', 't5']]
+        >>> tag_vocab = ut.flat_unique(*tags_list)
+        >>> regex_map = [('t[3-4]', 'A9'), ('t0', 'a0')]
+        >>> unmapped = list(set(tag_vocab) - set(alias_map.keys()))
+    """
+    import utool as ut
+    import re
+    alias_map = {}
+    for pat, new_tag in reversed(regex_map):
+        flags = [re.match(pat, t) for t in tag_vocab]
+        for old_tag in ut.compress(tag_vocab, flags):
+            alias_map[old_tag] = new_tag
+    return alias_map
+
+
+def alias_tags(tags_list, alias_map):
+    """
+    update tags to new values
+
+    Args:
+        tags_list (list):
+        alias_map (list): list of 2-tuples with regex, value
+
+    Returns:
+        list: updated tags
+
+    CommandLine:
+        python -m utool.util_tags alias_tags --show
+
+    Example:
+        >>> # DISABLE_DOCTEST
+        >>> from utool.util_tags import *  # NOQA
+        >>> import utool as ut
+        >>> tags_list = [['t1', 't2'], [], ['t3'], ['t4', 't5']]
+        >>> ut.build_alias_map()
+        >>> result = alias_tags(tags_list, alias_map)
+        >>> print(result)
+    """
+    def _alias_dict(tags):
+        tags_ = [alias_map.get(t, t) for t in tags]
+        return list(set([t for t in tags_ if t is not None]))
+    tags_list_ = [_alias_dict(tags) for tags in tags_list]
+    return tags_list_
+    # def _fix_tags(tags):
+    #     return {six.text_type(t.lower()) for t in tags}
+    # tags_list_ = list(map(_fix_tags, tags_list))
+    # re_list = [re.compile(pat) for pat, val in alias_map]
+    # val_list = ut.take_column(alias_map, 0)
+    # def _alias_regex(tags):
+    #     new_tags = 0
+    #     for t in tags:
+    #         matched = [re_.match(t) is not None for re_ in re_list]
+    #         matched_idx = ut.where(matched)
+    #         assert len(matched_idx) <= 1, 'more than one tag in %r matched pattern' % (tags,)
+    #         if len(matched_idx) > 0:
+    #             repl_tags = ut.take(val_list, matched_idx)
+    #             new_tags.extend(repl_tags)
+    #         else:
+    #             new_tags.append(t)
+    #     return new_tags
+    # # tags_list_ = [_alias_regex(tags) for tags in tags_list_]
+    # return tags_list_
+
+
 def filterflags_general_tags(tags_list, has_any=None, has_all=None,
                              has_none=None, min_num=None, max_num=None,
                              any_startswith=None, any_endswith=None,
@@ -83,7 +153,7 @@ def filterflags_general_tags(tags_list, has_any=None, has_all=None,
     import numpy as np
     import utool as ut
 
-    def fix_tags(tags):
+    def _fix_tags(tags):
         if ignore_case:
             return set([]) if tags is None else {six.text_type(t.lower()) for t in tags}
         else:
@@ -102,7 +172,7 @@ def filterflags_general_tags(tags_list, has_any=None, has_all=None,
         'or': np.zeros,
     }[logic]
 
-    tags_list_ = [fix_tags(tags_) for tags_ in tags_list]
+    tags_list_ = [_fix_tags(tags_) for tags_ in tags_list]
     flags = default_func(len(tags_list_), dtype=np.bool)
 
     if min_num is not None:
@@ -114,56 +184,56 @@ def filterflags_general_tags(tags_list, has_any=None, has_all=None,
         logic_func(flags, flags_, out=flags)
 
     if has_any is not None:
-        has_any = fix_tags(set(ut.ensure_iterable(has_any)))
+        has_any = _fix_tags(set(ut.ensure_iterable(has_any)))
         flags_ = [len(has_any.intersection(tags_)) > 0 for tags_ in tags_list_]
         logic_func(flags, flags_, out=flags)
 
     if has_none is not None:
-        has_none = fix_tags(set(ut.ensure_iterable(has_none)))
+        has_none = _fix_tags(set(ut.ensure_iterable(has_none)))
         flags_ = [len(has_none.intersection(tags_)) == 0 for tags_ in tags_list_]
         logic_func(flags, flags_, out=flags)
 
     if has_all is not None:
-        has_all = fix_tags(set(ut.ensure_iterable(has_all)))
+        has_all = _fix_tags(set(ut.ensure_iterable(has_all)))
         flags_ = [len(has_all.intersection(tags_)) == len(has_all) for tags_ in tags_list_]
         logic_func(flags, flags_, out=flags)
 
-    def test_item(tags_, fields, op, compare):
+    def _test_item(tags_, fields, op, compare):
         t_flags = [any([compare(t, f) for f in fields]) for t in tags_]
         num_passed = sum(t_flags)
         flag = op(num_passed, 0)
         return flag
 
-    def flag_tags(tags_list, fields, op, compare):
-        flags = [test_item(tags_, fields, op, compare) for tags_ in tags_list_]
+    def _flag_tags(tags_list, fields, op, compare):
+        flags = [_test_item(tags_, fields, op, compare) for tags_ in tags_list_]
         return flags
 
-    def execute_filter(flags, tags_list, fields, op, compare):
+    def _exec_filter(flags, tags_list, fields, op, compare):
         if fields is not None:
             fields = ut.ensure_iterable(fields)
             if ignore_case:
                 fields = [f.lower() for f in fields]
-            flags_ = flag_tags(tags_list, fields, op, compare)
+            flags_ = _flag_tags(tags_list, fields, op, compare)
             logic_func(flags, flags_, out=flags)
         return flags
 
-    flags = execute_filter(
+    flags = _exec_filter(
         flags, tags_list, any_startswith,
         operator.gt, six.text_type.startswith)
 
-    flags = execute_filter(
+    flags = _exec_filter(
         flags, tags_list, in_any,
         operator.gt, operator.contains)
 
-    flags = execute_filter(
+    flags = _exec_filter(
         flags, tags_list, any_endswith,
         operator.gt, six.text_type.endswith)
 
-    flags = execute_filter(
+    flags = _exec_filter(
         flags, tags_list, any_match,
         operator.gt, lambda t, f: re.match(f, t))
 
-    flags = execute_filter(
+    flags = _exec_filter(
         flags, tags_list, none_match,
         operator.eq, lambda t, f: re.match(f, t))
     return flags
