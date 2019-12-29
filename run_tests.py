82618406496a8f98f8ab05f127835965b32fd763
joncrall <erotemic@gmail.com>
2019-12-29 18:45:23 -0500

    wip

--- run_tests.py
+++ run_tests.py
@@ -1,14 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-from __future__ import absolute_import, division, print_function
-import utool as ut
-import sys
-
-
 if __name__ == '__main__':
-    import utool.tests.run_tests
-    import multiprocessing
-    multiprocessing.freeze_support()
-    ut.change_term_title('RUN UTOOL TESTS')
-    retcode = utool.tests.run_tests.run_tests()
-    sys.exit(retcode)
+    import pytest
+    import sys
+    package_name = 'utool'
+    pytest_args = [
+        '--cov-config', '.coveragerc',
+        '--cov-report', 'html',
+        '--cov-report', 'term',
+        '--xdoctest',
+        '--cov=' + package_name,
+    ]
+    pytest_args = pytest_args + sys.argv[1:]
+    pytest.main(pytest_args)
+
+
+# OLD UTOOL TESTER (still works)
+# if __name__ == '__main__':
+#     import utool.tests.run_tests
+#     import multiprocessing
+#     multiprocessing.freeze_support()
+#     ut.change_term_title('RUN UTOOL TESTS')
+#     retcode = utool.tests.run_tests.run_tests()
+#     sys.exit(retcode)
